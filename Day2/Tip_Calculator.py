f8edfb82ca221fabd02204dcf2b8349064bb1251
LukasH <lukas.hofmann77@gmail.com>
2022-10-30 22:33:19 +0100

    Typo

--- Day2/Tip_Calculator.py
+++ Day2/Tip_Calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator")
 
-bill = float(input("What was the total bill? $"))
+bill = float(input("What is the total bill? $"))
 tip = float(input("What kind of tip do you want to give? The answer will be interpreted as a percentage. "))
 people = int(input("How many people split the bill? "))
 price_each = (bill * (1 + tip / 100) / people)
