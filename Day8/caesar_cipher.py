a91a668dcbe8d0f0ddfb3804841873f052853b1b
LukasH <lukas.hofmann77@gmail.com>
2023-05-08 20:43:28 +0200

    Finished Caesar Cipher

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,3 +1,5 @@
+from caesar_cipher_art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
@@ -6,23 +8,28 @@ def caesar(text, shift, encrypt):
     # shift forward to encrypt, backward to decrypt
     if not encrypt:
         shift *= -1
-    for letter in text:
-        if letter in alphabet:
-            pos = alphabet.index(letter)
+    for char in text:
+        if char in alphabet:
+            pos = alphabet.index(char)
             new_pos = (pos + shift) % alphabet.__len__()
             res += alphabet[new_pos]
         else:
-            res += letter
+            res += char
     print(f"Transformed message: {res}")
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text_in = input("Type your message:\n").lower()
-shift_in = int(input("Type the shift number:\n"))
+print(logo)
+running = True
+while running:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text_in = input("Type your message:\n").lower()
+    shift_in = int(input("Type the shift number:\n"))
 
-if direction == "encode":
-    caesar(text_in, shift_in, True)
-elif direction == "decode":
-    caesar(text_in, shift_in, False)
-else:
-    print("Invalid input!")
+    if direction == "encode":
+        caesar(text_in, shift_in, True)
+    elif direction == "decode":
+        caesar(text_in, shift_in, False)
+    else:
+        print("Invalid input!")
+
+    running = input("Type '1' to restart, '0' to terminate.\n") == '1'
